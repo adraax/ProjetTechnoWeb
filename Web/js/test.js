2795e4db3e8efd7f6b6764f3fdbc694be7ecadf8
Adraax <merlin.olivier71@gmail.com>
2016-05-14 19:34:18 +0200

    Début debug event jquery

--- Web/js/test.js
+++ Web/js/test.js
@@ -1,35 +1,16 @@
 $(function() {
 	$('#mod').load('/connectionajax', function() {
 		console.log("Fenêtre de connexion modale chargée !");
-		
-	});	
+	});
+	
+	$('#myModal').on('click', 'button', function(event){
+		event.preventDefault();
+		var username = $('#username').val();
+		var password = $('#password').val();
+		console.log("prevent default modal "+username+" "+password);
+		console.log("content : "+$('#content').val());
+		$('#mod').load('/connectionajax', {username: username, password: password}, function() {
+			console.log('connexion');
+		});
+	});
 });
-
-setTimeout(function() {
-	$('#myModal button').click(function(event){
-			event.preventDefault();
-			var username = $('#username').val();
-			var password = $('#password').val();
-			console.log("prevent default modal "+username+" "+password);
-			console.log("content : "+$('#content').val());
-			$('#mod').load('/connectionajax', {username: username, password: password}, function() {
-				console.log('connexion');
-			});
-		});
-}, 1000);
-
-var listener = function() {
-	$('#myModal button').click(function(event){
-			event.preventDefault();
-			var username = $('#username').val();
-			var password = $('#password').val();
-			console.log("prevent default modal "+username+" "+password);
-			console.log("content : "+$('#content').val());
-			$('#mod').load('/connectionajax', {username: username, password: password}, function() {
-				console.log('connexion');
-			});
-		});
-		setTimeout(listener(), 500);
-};
-
-setTimeout(listener(), 1000);
