d938818d842e8a4928f385fb0c62b7aad03fa7e1
Adraax <merlin.olivier71@gmail.com>
2016-05-15 10:05:40 +0200

    Début chargement navbar auto en ajax

---
+++ App/Frontend/Modules/Ajax/AjaxController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Frontend\Modules\Profil;
+
+use \GJLMFramework\BaseController;
+use \GJLMFramework\HTTPRequest;
+
+class AjaxController extends BaseController
+{
+    public function getNavbarAction(HTTPRequest $request)
+    {
+        
+    }
+}
