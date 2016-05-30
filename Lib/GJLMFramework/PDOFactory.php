596aa59699e3a08aeaa5069d745eae1a44676908
Adraax <merlin.olivier71@gmail.com>
2016-05-30 17:37:39 +0200

    s

--- Lib/GJLMFramework/PDOFactory.php
+++ Lib/GJLMFramework/PDOFactory.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
     /*public static function getMysqlConnection($host, $name, $user, $password)
     {
-        $db = new \PDO('mysql:host'.$host.';dbname='.$name, $user, $password);
+        $db = new \PDO('"mysql:host'.$host.';dbname='.$name.'"', $user, $password);
         $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         return $db;
